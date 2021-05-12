df59471c47f67ec3cba510532c13924ce595dfa1
Kim Seojin <001106ksj@gmail.com>
2021-05-12 11:36:43 +0900

    [Feat] 툴바 추가

--- app/src/main/java/com/toystory/notget/MainActivity.kt
+++ app/src/main/java/com/toystory/notget/MainActivity.kt
@@ -2,6 +2,9 @@ package com.toystory.notget
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.toystory.notget.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initActionBar()
+    }
+
+    private fun initActionBar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_edit_30)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actionbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.btn_create_note -> {
+                Toast.makeText(this@MainActivity, "생성", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> {
+                Toast.makeText(this@MainActivity, "수정", Toast.LENGTH_SHORT).show()
+                true
+            }
+        }
     }
 }
